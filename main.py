319ea46bc9637fd7ce0045d7fbc13ba312e8d548
Harith Abeysinghe <e19003@eng.pdn.ac.lk>
2023-05-27 17:00:09 +0530

    Adding images to window

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("New Game")
 background_color = (25, 0, 100)
 FPS = 60
+SpaceShip = (50,40)
 
+Yellow_Spaceship_Image = pygame.image.load(os.path.join('Images','spaceship_yellow.png'))
+Yellow_Spaceship = pygame.transform.rotate(pygame.transform.scale(Yellow_Spaceship_Image, SpaceShip), 90)
+Red_Spaceship_Image = pygame.image.load(os.path.join('Images','spaceship_red.png'))
+Red_Spaceship = pygame.transform.rotate(pygame.transform.scale(Red_Spaceship_Image, SpaceShip),-90)
 
 def draw_window():
     WINDOW.fill(background_color)
+    WINDOW.blit(Yellow_Spaceship, (100,250))
+    WINDOW.blit(Red_Spaceship, (700,250))
     pygame.display.update()
 
 def main():

f2384b6d2dec07554448dd84d7198f532160b14b
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-07-25 00:03:21 +0530

    UI Changes

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ import pygame
 WIDTH, HEIGHT = 900,500
 WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("First Game")
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
+
+#Colors
+RED = (255,0,0)
+GREEN = (0, 255, 0)
+BLUE =  (0,0,255)
+#Frame Rate
+FPS = 60
+
+#Players
+YELLOW_SPACESHIP_IMAGE = pygame.image.load("D:\Python\Pygame\Assets\spaceship_yellow.png")
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
+RED_SPACESHIP_IMAGE = pygame.image.load("D:\Python\Pygame\Assets\spaceship_red.png")
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
+def draw_window():
+    WINDOW.fill(BLUE)
+    WINDOW.blit(YELLOW_SPACESHIP, (300,250))
+    WINDOW.blit(RED_SPACESHIP, (600, 250))
+    pygame.display.update()
 def main():
-    #Game Loop
+    clock = pygame.time.Clock()
     run = True
+
+    # Game Loop
     while run:
+        clock.tick(FPS) #Adding Frame Rate
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw_window()
     pygame.quit()
 
 if __name__ == "__main__":
